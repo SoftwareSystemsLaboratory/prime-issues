569301d065f61947b5e9b3a48b66e69b73a42a21
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-20 20:02:28 -0500

    Store the number of the commit

--- gh-all-issues.py
+++ gh-all-issues.py
@@ -9,9 +9,9 @@ def getGHIssues(
     filename: str = "issues.json",
 ) -> int:
     if repo == "":
-        command: str = f'gh issue list --json "closedAt,createdAt,id,state" --limit {limit} --state {state} --search "sort:created-asc"> {filename}'
+        command: str = f'gh issue list --json "closedAt,createdAt,id,number,state" --limit {limit} --state {state} --search "sort:created-asc"> {filename}'
     else:
-        command: str = f'gh issue list --repo {repo} --json "closedAt,createdAt,id,state" --limit {limit} --state {state} --search "sort:created-asc" > {filename}'
+        command: str = f'gh issue list --repo {repo} --json "closedAt,createdAt,id,number,state" --limit {limit} --state {state} --search "sort:created-asc" > {filename}'
 
     return call(command, shell=True)
 
@@ -19,3 +19,6 @@ def getGHIssues(
 def loadJSON(filename: str = "issues.json") -> dict:
     with open(file=filename, mode="r") as json:
         return load(json)
+
+
+getGHIssues(repo="numpy/numpy", limit=10)
