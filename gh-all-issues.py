9af11fac4d8e13592050a2476c1840f30812acaf
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-20 22:09:18 -0500

    Copy in get_argparse from git-all-python-cli-only

--- gh-all-issues.py
+++ gh-all-issues.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from json import load
 from subprocess import call
@@ -5,6 +6,35 @@ from subprocess import call
 from dateutil.parser import parse
 from intervaltree import IntervalTree
 
+def get_argparse() -> ArgumentParser:
+    parser: ArgumentParser = ArgumentParser(
+        prog="Git All Python (CLI Only)",
+        usage="This program outputs the lines of code (LOC) per commit and the delta LOC of a Git repository in JSON format.",
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        help="Directory containing repository root folder (.git)",
+        default=".",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "-b",
+        "--branch",
+        help="Default branch for analysis to be ran on",
+        default="main",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "-s",
+        "--save-json",
+        help="Save analysis to JSON file (EX: --save-json=output.json)",
+        type=str,
+        required=True,
+    )
+    return parser
 
 def getGHIssues(
     repo: str = "",
@@ -43,3 +73,5 @@ def createIntervalTree(data: list) -> IntervalTree:
         tree.addi(begin=begin, end=end, data=issue)
 
     return tree
+
+if __name__ == "__main__":
