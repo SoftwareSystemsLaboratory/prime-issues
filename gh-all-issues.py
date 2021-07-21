8e9f0c4d875615ceb1ca74e8dd8c1dbef537ed3c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-20 19:25:37 -0500

    Write method to get a JSON file of issues

--- gh-all-issues.py
+++ gh-all-issues.py
@@ -1,5 +1,5 @@
 from os import popen
-from os import system
+from subprocess import call
 
 
 def getGHIssues(
@@ -7,9 +7,10 @@ def getGHIssues(
     limit: int = 10000,
     state: str = "all",
     filename: str = "issues.json",
-) -> None:
-    filenameSuffix: str = filename.split(".")[-1]
-    if filenameSuffix != "json":
-        print("Invalid filename. Needs to be a JSON file.")
-        quit(1)
-    command: str = f'gh issue list --repo {repo} --json "closedAt,createdAt,id,state" --limit {limit} --state {state} > {filename}'
+) -> int:
+    if repo == "":
+        command: str = f'gh issue list --json "closedAt,createdAt,id,state" --limit {limit} --state {state} > {filename}'
+    else:
+        command: str = f'gh issue list --repo {repo} --json "closedAt,createdAt,id,state" --limit {limit} --state {state} > {filename}'
+
+    return call(command, shell=True)
