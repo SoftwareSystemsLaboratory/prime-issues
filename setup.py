b0722e2ae0c5f5113001d4f6efc2463eebf56e62
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-11 17:36:46 -0500

    Update folder entrypoint in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # setup.py - placeholder for eventual setup script
-
 from setuptools import setup
 
 from ssl_metrics import version
 
 setup(
     name="ssl-metrics-github-issues",
-    packages=["ssl_metrics"],
+    packages=["ssl_metrics_github_issues"],
     version=version.version(),
     description="SSL Metrics - GitHub Issues Analysis",
     author="Software and Systems Laboratory - Loyola University Chicago",
@@ -19,7 +18,7 @@ setup(
         "Programming Language :: Python :: 3.0",
         "License :: OSI Approved :: Apache Software License",
     ],
-    python_requires=">=3",
+    python_requires=">=3.9",
     install_requires=[
         "intervaltree",
         "numpy",
@@ -31,9 +30,9 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "ssl-metrics-github-issues-collect = ssl_metrics.github_issues:main",
-            "ssl-metrics-github-issues-convert = ssl_metrics.convert_output:main",
-            "ssl-metrics-github-issues-graph = ssl_metrics.create_graph:main",
+            "ssl-metrics-github-issues-collect = ssl_metrics_github_issues.github_issues:main",
+            "ssl-metrics-github-issues-convert = ssl_metrics_github_issues.convert_output:main",
+            "ssl-metrics-github-issues-graph = ssl_metrics_github_issues.create_graph:main",
         ]
     },
 )
