831104e5c3c0ef9e7169e79f493c7f26856da11b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-11 13:44:25 -0600

    Import code from github_issues.py:

--- ssl_metrics_github_issues/main.py
+++ ssl_metrics_github_issues/main.py
@@ -1,4 +1,7 @@
 from argparse import Namespace, ArgumentParser
+from requests import Response, get
+from progress.bar import Bar
+from libs.common import getLastPage, storeJSON
 
 
 def getArguements() -> Namespace:
@@ -34,8 +37,7 @@ def getArguements() -> Namespace:
 
     issuesParser: ArgumentParser = subparser.add_parser(
         "issues",
-        prog="",
-        usage=False,
+        usage="Access the GitHub issues of a repository via the GitHub Issues REST API",
         description="",
     )
     issuesParser.add_argument(
@@ -63,5 +65,81 @@ def getArguements() -> Namespace:
     # )
     return parser.parse_args()
 
-from pprint import pprint as print
-print(getArguements().__dict__)
+def getGitHubIssues(
+    repo: str,
+    token: str,
+    pullRequests: bool = False,
+) -> int:
+
+    data: list = []
+    requestHeaders: dict = {
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "gh-all-issues",
+        "Authorization": f"token {token}",
+    }
+    urlTemplate: str = "https://api.github.com/repos/{}/issues?state=all&sort=created&direction=asc&per_page=100&page={}"
+
+    html: Response = get(url=urlTemplate.format(repo, 1), headers=requestHeaders)
+
+    requestIterations: int = getLastPage(response=html)
+
+    json: dict = html.json()
+    for index in range(len(json)):
+        if testIfPullRequest(json[index]) is False:
+            data.append(json[index])
+
+    if pullRequests is False:
+        barStr: str = (
+            f"Removing pull request issues and then storing issue data from {repo}... "
+        )
+    else:
+        barStr: str = f"Storing issue data from {repo}... "
+
+    barMax: int = requestIterations
+    with Bar(barStr, max=barMax) as bar:
+        bar.next()
+
+        if requestIterations != 1:
+            for iteration in range(requestIterations + 1):
+
+                if iteration > 1:
+                    apiCall: str = urlTemplate.format(repo, iteration)
+                    html: Response = get(url=apiCall, headers=requestHeaders)
+
+                    json: dict = html.json()
+                    for index in range(len(json)):
+                        if pullRequests is False:
+                            if testIfPullRequest(json[index]) is False:
+                                data.append(json[index])
+                        else:
+                            data.append(json[index])
+
+                    bar.next()
+    return data
+
+
+def testIfPullRequest(dictionary: dict) -> bool:
+    try:
+        dictionary["pull_request"]
+        return True
+    except KeyError:
+        return False
+
+
+def main() -> None:
+    args: Namespace = getArguements()
+
+    issues: list = getGitHubIssues(
+        repo=args.repository,
+        token=args.token,
+        pullRequests=args.pull_requests,
+    )
+
+    storeJSON(
+        json=issues,
+        filename=args.save_json,
+    )
+
+
+if __name__ == "__main__":
+    main()
