67877ae15732bc7104e3588e91781fa3b4874765
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-04 14:45:09 -0500

    Add entrypoint for gitlab issue collection

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
     entry_points={
         "console_scripts": [
             "ssl-metrics-github-issues-collect = ssl_metrics_github_issues.github:main",
+            "ssl-metrics-gitlab-issues-collect = ssl_metrics_github_issues.gitlab:main",
             "ssl-metrics-github-issues-graph = ssl_metrics_github_issues.graph:main",
         ]
     },
